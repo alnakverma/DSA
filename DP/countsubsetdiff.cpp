2d061f3e55aff315ecce43b86d8590238a43fbce
alnakverma <anujkumarverma1010@gmail.com>
2025-03-28 23:35:30 +0530

    update

--- DP/countsubsetdiff.cpp
+++ DP/countsubsetdiff.cpp
@@ -10,7 +10,7 @@ int countsubsetdiff(int arr[], int k, int n){
         }
     }
     for(int i=1; i<=n; i++){
-        for(int j=1; j<=k; j++){
+        for(int j=0; j<=k; j++){
             if(arr[i-1]<=j){
                 t[i][j] = t[i-1][j-arr[i-1]] + t[i-1][j];
             }
@@ -30,6 +30,7 @@ int main()
     for(int i=0; i<n; i++){
         sum+=arr[i];
     }
+    if((sum+diff) % 2 != 0 || sum < diff) cout<<0<<endl;
     int k = (sum + diff)/2;
     cout<<countsubsetdiff(arr,k,n)<<endl;
     return 0;
